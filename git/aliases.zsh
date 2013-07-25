d75194e69b5ca07bbe85047c451d92b3c58eb424
Konstantin Sokhan <git@ksokhan.com>
2013-07-25 17:41:19 -0400

    updating git stuff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,7 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s -%Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
+alias gd='git diff --color-words'
 alias gc='git commit -m'
 alias gca='git commit -am'
 alias gco='git checkout'
